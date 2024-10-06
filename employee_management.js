d0844c70aa54669be8873becb700eb3d12dbc828
Myia Lamy <mlamy@usf.edu>
2024-10-06 00:52:59 -0400

    Create Department class to manage employees and calculate salary & Handle bonuses in the department salary calculation

--- employee_management.js
+++ employee_management.js
@@ -12,3 +12,26 @@ class Employee { // Define employee class.
         return `${this.name} position is ${this.position} with a salary of $${this.salary}.`;
     }
 }
+
+// Task 2: Create a Department Class
+
+class Department { // Define Department class. 
+    constructor(name) {
+        this.name = name;
+        this.employees = [];
+    }
+    addEmployee(employee) { // Used to add an employee to the department. 
+        this.employees.push(employee); // Pushes/add employee to employees array.
+    }
+    getDepartmentSalary() { // Returns the total salary of all employees in the department. 
+        return this.employees.reduce((total, employee) => total + employee.salary, 0); // Calculation to add all employees salaries.
+    }
+    calculateTotalSalaryWithBonus(){ // Task 4: Handle Bonuses for Managers
+
+        return this.employees.reduce((total, employee)=> { 
+            if (employee instanceof Manager) {
+                return total + employee.salary + employee.bonus; // Add salary to the bonus if the employee is in a manager position.  
+            }
+            return total + employee.salary; }, 0); // Used to add salary if the employee is not a manager. 
+    }
+}
